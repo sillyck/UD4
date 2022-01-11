ba6f52a1ea0e8ddb94d4f15acb258adc839ba754
sillyck <ribellas2002@gmail.com>
2022-01-11 13:04:42 +0100

    activitats

---
+++ src/JavaBasic1App.java
@@ -0,0 +1,20 @@
+public class JavaBasic1App {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		short n1 = 10;
+		short n2 = 5;
+		int suma=n1+n2;
+		int resta=n1-n2;
+		int mult=n1*n2;
+		int divis=n1/n2;
+		int mod=n1%n2;
+		
+		System.out.println(suma);
+		System.out.println(resta);
+		System.out.println(mult);
+		System.out.println(divis);
+		System.out.println(mod);
+	}
+
+}
---
+++ src/JavaBasic2App.java
@@ -0,0 +1,15 @@
+public class JavaBasic2App {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int n = 10;
+		double a = 10.2;
+		char c = 'J';
+		
+		System.out.println(n + " " + a + " "+ c);
+		System.out.println(n+a);
+		System.out.println(a-n);
+		System.out.println(c);
+	}
+
+}
